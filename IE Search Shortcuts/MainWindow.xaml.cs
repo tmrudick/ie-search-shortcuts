c21d32394586467f84e51e01a942f13eab052546
Tom Rudick <tmrudick@gmail.com>
2011-06-27 19:54:18 -0700

    Adding text boxes to add a shortcut.

--- IE Search Shortcuts/MainWindow.xaml.cs
+++ IE Search Shortcuts/MainWindow.xaml.cs
@@ -19,9 +19,79 @@ namespace IE_Search_Shortcuts
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool nameChanged, keywordChanged, urlChanged;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void nameTxtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameChanged == false)
+            {
+                nameTxtBox.Text = "";
+            }
+        }
+
+        private void keywordTxtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (keywordChanged == false)
+            {
+                keywordTxtBox.Text = "";
+            }
+        }
+
+        private void urlTxtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (urlChanged == false)
+            {
+                urlTxtBox.Text = "";
+            }
+        }
+
+        private void nameTxtBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameTxtBox.Text != "")
+            {
+                nameChanged = true;
+            }
+        }
+
+        private void keywordTxtBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (keywordTxtBox.Text != "")
+            {
+                keywordChanged = true;
+            }
+        }
+
+        private void urlTxtBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (urlTxtBox.Text != "")
+            {
+                urlChanged = true;
+            }
+        }
+
+        private void urlTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                AddShortcut();
+                shortcutListBox.Focus();
+            }
+        }
+
+        private void AddShortcut()
+        {
+            
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddShortcut();
+            shortcutListBox.Focus();
         }
     }
 }
